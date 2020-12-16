c2494d2f694c21bd52cd3c0b7ccfa0413aea7193
Gbolahan <yungest0@gmail.com>
2020-12-16 19:34:01 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+// The compiler concatinated this code because of the 
+// the plus sign  after the string.
+let x = "42" + 7;
+console.log("X is " + x)
+let y = "42" - 7;
+console.log("Y is " + y)
